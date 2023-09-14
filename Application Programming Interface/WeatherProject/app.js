d7ea438a53b4d817ba3a20f552425afc7d21047d
Mansij Ranjit <mansijranjit@gmail.com>
2023-09-15 00:41:03 +0545

    Day 6

--- Application Programming Interface/WeatherProject/app.js
+++ Application Programming Interface/WeatherProject/app.js
@@ -16,18 +16,33 @@ const intro = {
 
 */
 
+///////Give weather information to user as per their input city using external api///////
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-  //From site link https://openweathermap.org/current
-  const url =
-    "https://api.openweathermap.org/data/2.5/weather?q=Nepal,Kathmandu&appid=6280234d12c32699496cd23bed2fd2a0&units=metric";
-  https.get(url, function (response) {
-    //console.log(response.headers);
+  res.sendFile(__dirname + "/index.html");
+});
 
+app.post("/", function (req, res) {
+  //From site link https://openweathermap.org/current
+  const city = req.body.cityName;
+  //console.log(req.body);
+  const apiKey = "6280234d12c32699496cd23bed2fd2a0";
+  const unit = "metric";
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&appid=" +
+    apiKey +
+    "&units=" +
+    unit;
+
+  https.get(url, function (response) {
     response.on("data", function (data) {
       const weatherData = JSON.parse(data);
       //console.log(weatherData);
@@ -37,7 +52,7 @@ app.get("/", function (req, res) {
       const icon = weatherData.weather[0].icon;
       const imageUrl = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
 
-      res.write(`<h1>Todays temperature is : ${temp}</h1>`);
+      res.write(`<h1>Todays temperature of ${city} is : ${temp}</h1>`);
       res.write("<p>The weather description is " + weatherDescription + "</p>");
       res.write("<img src=" + imageUrl + ">");
       res.send();
