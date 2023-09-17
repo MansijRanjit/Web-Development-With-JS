08bf48b85c11d3872e21572a62a753eace4eb3ba
Mansij Ranjit <mansijranjit@gmail.com>
2023-09-17 09:06:48 +0545

    Day 7

--- Application Programming Interface/Newsletter-Signup/app.js
+++ Application Programming Interface/Newsletter-Signup/app.js
@@ -5,8 +5,19 @@ const request = require("request");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(express.static("public"));
+
 app.get("/", function (req, res) {
-  res.send("Listen on port 3000");
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+  const password = req.body.password;
+
+  console.log(firstName, lastName, email, password);
 });
 
 app.listen(3000, function () {
