373952ae5ef6bc30495861d6fe2fe5eeecb9d235
Mansij Ranjit <mansijranjit@gmail.com>
2023-09-18 23:55:52 +0545

    Day 6

--- Application Programming Interface/Newsletter-Signup/app.js
+++ Application Programming Interface/Newsletter-Signup/app.js
@@ -17,9 +17,24 @@ app.post("/", function (req, res) {
   const email = req.body.email;
   const password = req.body.password;
 
-  console.log(firstName, lastName, email, password);
+  // console.log(firstName, lastName, email, password);
+
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+      },
+    ],
+  };
 });
 
 app.listen(3000, function () {
   console.log("Server is running on port 3000");
 });
+
+//API key
+//59c533e3fdc8d57da2f307df9770cb7e-us21
+
+//Audience Id or List Id
+//975e0b9f62
