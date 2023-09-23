e676c80cd64e8e0d4d7f24d7c63d21886b974b87
Mansij Ranjit <mansijranjit@gmail.com>
2023-09-23 23:56:38 +0545

    Day9

--- 4) EJS/todolist/app.js
+++ 4) EJS/todolist/app.js
@@ -2,20 +2,33 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.get("/", function (req, res) {
+//   let today = new Date();
+//   let currentDay = today.getDay();
+
+//   if (currentDay === 0 || currentDay === 6) {
+//     res.write("<h1>Holiday</h1>");
+//     res.write(`<p>${currentDay}</p>`);
+//   } else {
+//     res.sendFile(__dirname + "/index.html");
+//   }
+// });
 
 app.get("/", function (req, res) {
   let today = new Date();
   let currentDay = today.getDay();
 
   if (currentDay === 0 || currentDay === 6) {
-    res.write("<h1>Holiday</h1>");
-    res.write(`<p>${currentDay}</p>`);
+    res.render("list", { day: "Weekend" });
   } else {
-    res.sendFile(__dirname + "/index.html");
+    res.render("list", { day: "Weekday" });
   }
 });
+
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
