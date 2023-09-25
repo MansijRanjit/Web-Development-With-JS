a026f17b78f382ce49c5b487da1e7dcd875c3a66
Mansij Ranjit <mansijranjit@gmail.com>
2023-09-25 23:58:22 +0545

    Day 9

--- 4) EJS/todolist/app.js
+++ 4) EJS/todolist/app.js
@@ -20,13 +20,27 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
   let today = new Date();
-  let currentDay = today.getDay();
+  //let currentDay = today.getDay();
 
-  if (currentDay === 0 || currentDay === 6) {
-    res.render("list", { day: "Weekend" });
-  } else {
-    res.render("list", { day: "Weekday" });
-  }
+  // if (currentDay === 0) {
+  //   res.render("list", { day: "Sunday" });
+  // } else if (currentDay === 1) {
+  //   res.render("list", { day: "Monday" });
+  // } else if (currentDay === 2) {
+  //   res.render("list", { day: "Tuesday" });
+  // } else if (currentDay === 3) {
+  //   res.render("list", { day: "Wednesday" });
+  // } else if (currentDay === 4) {
+  //   res.render("list", { day: "Thursday" });
+  // } else if (currentDay === 5) {
+  //   res.render("list", { day: "Friday" });
+  // } else if (currentDay === 6) {
+  //   res.render("list", { day: "Saturday" });
+  // }
+
+  const options = { weekday: "long", month: "long", day: "numeric" };
+  const day1 = today.toLocaleDateString("en-US,options");
+  res.render("list", { day: day1 });
 });
 
 app.listen(3000, function () {
